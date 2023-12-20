5dbaf192e6211bfe4bd9ff063d4794f9942ea43e
ComeSFI <comesfi@LAPTOP-CUJEPKDC>
2023-12-20 15:10:55 +0100

    step 8

--- services/epf-flower-data-science/src/api/router.py
+++ services/epf-flower-data-science/src/api/router.py
@@ -1,8 +1,18 @@
 """API Router for Fast API."""
 from fastapi import APIRouter
+from fastapi.responses import RedirectResponse
 
 from src.api.routes import hello
+from src.api.routes import data
 
 router = APIRouter()
 
+@router.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
+
+
 router.include_router(hello.router, tags=["Hello"])
+
+router.include_router(data.router, tags=["Download Data"])
---
+++ services/epf-flower-data-science/src/api/routes/data.py
@@ -0,0 +1,36 @@
+import kaggle
+from fastapi import APIRouter
+from src.schemas.message import MessageResponse
+import pandas as pd
+
+router = APIRouter()
+
+
+
+@router.get("/download-dataset")
+async def download_dataset():
+    # The authenticate method no longer requires 'username' and 'key' parameters
+    kaggle.api.authenticate()
+    
+    dataset_name = "uciml/iris"
+    save_dir = "services\epf-flower-data-science\src\data"
+    kaggle.api.dataset_download_files(dataset_name, path=save_dir, unzip=True)
+
+    return {"message": "Dataset downloaded and saved successfully."}
+
+@router.get("/load-data")
+def load_data():
+    file_path = "services\epf-flower-data-science\src\data\Iris.csv"
+    df = pd.read_csv(file_path)
+    return df.to_json(orient="records")
+
+@router.get("/preprocess")
+def preprocess_data():
+    data = load_data()
+    data = data.replace("Iris-", "")
+    data = data.replace("Cm","(Cm)")
+    return data
+
+@router.get("/split")
+def train_test_split():
+    data  = pd.read_json
