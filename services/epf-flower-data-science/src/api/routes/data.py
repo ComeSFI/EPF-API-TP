6a8b958e3d2252d5d794b0cff827cf2bc1a52f29
ComeSFI <comesfi@LAPTOP-CUJEPKDC>
2024-01-06 18:58:41 +0100

    Model save

--- services/epf-flower-data-science/src/api/routes/data.py
+++ services/epf-flower-data-science/src/api/routes/data.py
@@ -1,8 +1,13 @@
 import kaggle
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Request
 # from src.schemas.message import MessageResponse
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import json
+from sklearn.svm import SVC
+from sklearn.preprocessing import LabelEncoder
+import joblib
+import os
 
 router = APIRouter()
 
@@ -38,5 +43,77 @@ def train_test_split_func():
     train, test = train_test_split(data, test_size=0.2)
     return train.to_json(orient="records"), test.to_json(orient="records")
 
+@router.get("/init-params")
+def init_params():
+    model = SVC()
+    model_params = model.get_params()
+    file_path = 'services\epf-flower-data-science\src\config\model_parameters.json'
 
+    # Save the model parameters to a JSON file
+    with open(file_path, 'w') as file:
+        json.dump(model_params, file, indent=2)
+
+@router.get("/train-model")
+def train_model():
+    try:
+        train, _ = train_test_split_func()
+        train_data = pd.read_json(train)
+        
+        # Extract features and labels
+        features = train_data.drop(columns=["Species"])
+        labels = train_data["Species"]
+
+        # Convert labels to numeric format using LabelEncoder
+        label_encoder = LabelEncoder()
+        labels_encoded = label_encoder.fit_transform(labels)
+
+        # Initialize the SVC classifier
+        model = SVC()
+
+        # Train the model
+        model.fit(features, labels_encoded)
+        if not os.path.exists('services/epf-flower-data-science/src/models'):
+            os.makedirs('services/epf-flower-data-science/src/models')
+        # Save the trained model to the models folder
+        model_filename = 'services/epf-flower-data-science/src/models/trained_model.joblib'
+        joblib.dump(model, model_filename)
+
+        return {"message": "Model trained and saved successfully"}
+
+    except Exception as e:
+        return {"error": str(e)}
+    
+@router.get("/predict")
+def predict():
+    try:
+        train, _ = train_test_split_func()
+        train_data = pd.read_json(train)
+
+        # Extract features and labels
+        features = train_data.drop(columns=["Species"])
+        labels = train_data["Species"]
+
+        # Convert labels to numeric format using LabelEncoder
+        label_encoder = LabelEncoder()
+        labels_encoded = label_encoder.fit_transform(labels)
+
+        # Initialize the SVC classifier
+        model = SVC()
+
+        # Train the model
+        model.fit(features, labels_encoded)
+        if not os.path.exists('services/epf-flower-data-science/src/models'):
+            os.makedirs('services/epf-flower-data-science/src/models')
+        # Save the trained model to the models folder
+        model_filename = 'services/epf-flower-data-science/src/models/trained_model.joblib'
+        joblib.dump(model, model_filename)
+
+        return {"message": "Model trained and saved successfully"}
+    
+        
+
+    except Exception as e:
+        return {"error": str(e)}
+    
+    
     
